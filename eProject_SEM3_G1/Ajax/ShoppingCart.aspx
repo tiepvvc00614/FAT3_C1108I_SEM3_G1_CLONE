c4690f9ea8f085707608d0a784de8ac926ba14c0
Vu Van Tiep <tiepvv_c00614@fpt.aptech.ac.vn>
2013-05-24 01:44:11 -0700

    update shopping cart

---
+++ eProject_SEM3_G1/Ajax/ShoppingCart.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ShoppingCart.aspx.cs" Inherits="eProject_SEM3_G1.Ajax.ShoppingCart" %>
--- eProject_SEM3_G1/Model/OrderDetails.cs
+++ eProject_SEM3_G1/Model/OrderDetails.cs
@@ -10,5 +10,42 @@ namespace eProject_SEM3_G1.Model
         private int orderDetailId;
         private int orderDetailQuantity;
         private Product orderDetailProduct;
+
+
+        public OrderDetails(Product pro, int quan)
+        {
+            this.orderDetailProduct = pro;
+            this.orderDetailQuantity = quan;
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return this.orderDetailQuantity;
+            }
+            set
+            {
+                this.orderDetailQuantity = value;
+            }
+        }
+
+        public float TotalPrice
+        {
+            get
+            {
+                float priceProduct = this.orderDetailProduct.ProductPrice;
+                return (priceProduct * this.orderDetailQuantity) - (((priceProduct * this.orderDetailQuantity) / 100) * this.orderDetailProduct.ProductDiscount);
+            }
+        }
+
+        public Product OrderDetailProduct
+        {
+            get { return orderDetailProduct; }
+        } 
+
+
+
+
     }
 }
